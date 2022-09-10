86e4ee8e559876fd9eaa87f81969e153970462e3
vivit <kenji@vivitjp.com>
2022-09-10 19:30:39 +0900

    Dynasty 型とデータ

---
+++ src/config/DynastyData.tsx
@@ -0,0 +1,39 @@
+import { Dynasty } from "../types/Dynasty"
+
+export const DynastyData: Dynasty[] = [
+  {
+    Id: 1,
+    name: "周",
+    year: -2000,
+  },
+  {
+    Id: 2,
+    name: "春秋戦国",
+    year: -1500,
+  },
+  {
+    Id: 3,
+    name: "秦",
+    year: -240,
+  },
+  {
+    Id: 4,
+    name: "漢",
+    year: -200,
+  },
+  {
+    Id: 5,
+    name: "新",
+    year: 0,
+  },
+  {
+    Id: 6,
+    name: "漢",
+    year: 20,
+  },
+  {
+    Id: 7,
+    name: "三国",
+    year: 220,
+  },
+]
---
+++ src/types/Dynasty.ts
@@ -0,0 +1,5 @@
+export type Dynasty = {
+  Id: number
+  name: string
+  year: number
+}
