3717337a161ca84bc1d7f04d7e848f7ab2f48340
vivit <kenji@vivitjp.com>
2022-09-10 19:30:06 +0900

    button general component 模索

--- src/Components/features/Card/HistoryCard.tsx
+++ src/Components/features/Card/HistoryCard.tsx
@@ -1,15 +1,36 @@
-import React, { FC, memo } from "react"
+import React, { FC, memo, useRef } from "react"
 import { HistoryData } from "../../../config/HistoryData"
+import { useDataContext } from "../../../context/DataProvider"
+import { ButtonRef } from "../../parts/ButtonRef"
 
 const HistoryCard: FC<HistoryData> = memo(
   ({ Id, name, yearBirth }): JSX.Element => {
+    const ref = useRef<HTMLButtonElement>(null!)
+
+    const { historyMap, setHistoryMap } = useDataContext()
+
+    const handler = () => {
+      const newMap = new Map(historyMap)
+      const obj = newMap.get(Id)
+      if (obj) {
+        obj.name = "新沼謙治"
+        newMap.set(Id, obj)
+        setHistoryMap(newMap)
+      }
+    }
+
     return (
       <div className="flex flex-col border p-4">
         <div className="flex border p-3 justify-center align-middle">
           {name}
         </div>
-        <div className="flex border p-3 justify-end align-middle">
+        <div className="flex border p-3 justify-center align-middle">
           {yearBirth}
+        </div>
+        <div className="flex border p-3 justify-center align-middle">
+          <ButtonRef ref={ref} customClass={"text-red-500"} onClick={handler}>
+            Change
+          </ButtonRef>
         </div>
       </div>
     )
--- src/Components/parts/ButtonRef.tsx
+++ src/Components/parts/ButtonRef.tsx
@@ -1,10 +1,20 @@
 import React, { forwardRef } from "react"
+import clsx from "clsx"
 
-type Props = React.ComponentProps<"button">
+type Props = React.ComponentProps<"button"> & {
+  customClass: string
+}
 //type Props = React.ComponentPropsWithRef<"button">;
 
+const className =
+  "p-3 text-center border border-gray-100 rounded-md shadow-md shadow-gray-200"
+
 export const ButtonRef = forwardRef<HTMLButtonElement, Props>(
-  ({ children }, ref) => {
-    return <button ref={ref}>{children}</button>
+  ({ children, customClass, ...rest }, ref) => {
+    return (
+      <button ref={ref} className={clsx(customClass, className)} {...rest}>
+        {children}
+      </button>
+    )
   }
 )
